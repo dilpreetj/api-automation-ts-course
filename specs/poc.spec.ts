e8d7fe08802717c0210bc3bac696b7f4c1fd52dc
Dilpreet Johal <automationbro@gmail.com>
2023-03-11 18:01:58 -0500

    POC: GET /comments with query params

--- specs/poc.spec.ts
+++ specs/poc.spec.ts
@@ -7,4 +7,12 @@ describe('POC Tests', () => {
     expect(res.statusCode).toBe(200)
     expect(res.body[0].id).toBe(1);
   })
+
+  it('GET /comments with query params', async () => {
+    // const res = await request.get('/comments?postId=1')
+    const res = await request
+      .get('/comments')
+      .query({ postId: 1, limit: 10 })
+    expect(res.body[0].postId).toBe(1)
+  });
 })
