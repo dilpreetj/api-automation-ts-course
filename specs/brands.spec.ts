647f1c30d407f216450c8255238d54328c4d20e2
Dilpreet Johal <dilpreet@sdetunicorns.com>
2023-04-07 19:17:10 -0400

    get /brand/:id

--- specs/brands.spec.ts
+++ specs/brands.spec.ts
@@ -8,4 +8,10 @@ describe('Brands', () => {
     expect(res.body.length).toBeGreaterThan(1);
     expect(Object.keys(res.body[0])).toEqual(['_id', 'name']);
   });
+
+  it('GET /brand/:id', async () => {
+    const res = await request.get('/brands/63448f0500b2931578c0a5b1');
+    expect(res.statusCode).toEqual(200);
+    expect(res.body.name).toEqual('Apple')
+  });
 });
