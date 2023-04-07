f40a94a620560fd95570f0dc87c724a941c27ef0
Dilpreet Johal <dilpreet@sdetunicorns.com>
2023-04-07 19:29:56 -0400

    post /brands

--- specs/brands.spec.ts
+++ specs/brands.spec.ts
@@ -2,16 +2,36 @@ import * as supertest from 'supertest';
 const request = supertest('https://sdetunicorns.com/api/test')
 
 describe('Brands', () => {
-  it('GET /brands', async () => {
-    const res = await request.get('/brands');
-    expect(res.statusCode).toEqual(200);
-    expect(res.body.length).toBeGreaterThan(1);
-    expect(Object.keys(res.body[0])).toEqual(['_id', 'name']);
+
+  describe('Fetch brands', () => {
+    it('GET /brands', async () => {
+      const res = await request.get('/brands');
+      expect(res.statusCode).toEqual(200);
+      expect(res.body.length).toBeGreaterThan(1);
+      expect(Object.keys(res.body[0])).toEqual(['_id', 'name']);
+    });
+
+    it('GET /brand/:id', async () => {
+      const res = await request.get('/brands/63448f0500b2931578c0a5b1');
+      expect(res.statusCode).toEqual(200);
+      expect(res.body.name).toEqual('Apple')
+    });
   });
 
-  it('GET /brand/:id', async () => {
-    const res = await request.get('/brands/63448f0500b2931578c0a5b1');
-    expect(res.statusCode).toEqual(200);
-    expect(res.body.name).toEqual('Apple')
+  describe('Create brands', () => {
+    it('POST /brands', async () => {
+      const data = {
+        'name': 'FitBit',
+        'description': 'FitBit Watches'
+      }
+      const res = await request
+        .post('/brands')
+        .send(data)
+
+      expect(res.statusCode).toEqual(200)
+      expect(res.body.name).toEqual(data.name)
+      expect(res.body).toHaveProperty('createdAt')
+    });
   });
+
 });
