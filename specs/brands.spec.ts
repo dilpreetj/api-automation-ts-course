f5dd730f1c1dcd02a8edddc7e30983a4252fb363
Dilpreet Johal <dilpreet@sdetunicorns.com>
2023-04-09 15:30:18 -0400

    fix /brands/:id uniqueness

--- specs/brands.spec.ts
+++ specs/brands.spec.ts
@@ -10,15 +10,10 @@ describe('Brands', () => {
       expect(res.body.length).toBeGreaterThan(1);
       expect(Object.keys(res.body[0])).toEqual(['_id', 'name']);
     });
-
-    it('GET /brand/:id', async () => {
-      const res = await request.get('/brands/63448f0500b2931578c0a5b1');
-      expect(res.statusCode).toEqual(200);
-      expect(res.body.name).toEqual('Apple')
-    });
   });
 
-  describe('Create brands', () => {
+  describe('Create & Fetch brands', () => {
+    let newBrand;
     it('POST /brands', async () => {
       const data = {
         'name': 'Test Brand ' + Math.floor(Math.random() * 100000),
@@ -31,6 +26,14 @@ describe('Brands', () => {
       expect(res.statusCode).toEqual(200)
       expect(res.body.name).toEqual(data.name)
       expect(res.body).toHaveProperty('createdAt')
+
+      newBrand = res.body;
+    });
+
+    it('GET /brand/:id', async () => {
+      const res = await request.get('/brands/' + newBrand._id);
+      expect(res.statusCode).toEqual(200);
+      expect(res.body.name).toEqual(newBrand.name)
     });
   });
 
