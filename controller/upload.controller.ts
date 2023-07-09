c3fe7febecea52a8929d7204242ec82f0e81743a
Dilpreet Johal <dilpreet@sdetunicorns.com>
2023-07-09 14:15:32 -0400

    added multiple files controller

--- controller/upload.controller.ts
+++ controller/upload.controller.ts
@@ -8,6 +8,18 @@ class UploadController {
       .post('/upload/single')
       .attach('single', filepath)
   }
+
+  postUploadMultiple(files: string[]) {
+    const req = request
+      .post('/upload/multiple')
+
+    files.forEach(file => {
+      req
+        .attach('multiple', file)
+    })
+
+    return req;
+  }
 }
 
 export default new UploadController();
