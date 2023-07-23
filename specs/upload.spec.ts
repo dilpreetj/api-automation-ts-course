1912a335fa93b0bf2ce412dcfc7b056511cdf0d8
Dilpreet Johal <dilpreet@sdetunicorns.com>
2023-07-23 14:23:29 -0400

    Added POST /upload/multiple test

--- specs/upload.spec.ts
+++ specs/upload.spec.ts
@@ -5,4 +5,15 @@ describe('Upload File', () => {
     const res = await controller.postUploadSingle('data/luffy.jpeg');
     expect(res.body.filename).toEqual('luffy.jpeg');
   });
+
+  it('POST /upload/multiple', async () => {
+    const files = [
+      'data/luffy.jpeg',
+      'data/zoro.jpg'
+    ]
+    const res = await controller.postUploadMultiple(files);
+    expect(res.body.length).toBe(2);
+    expect(res.body[0].filename).toEqual('luffy.jpeg');
+    expect(res.body[1].filename).toEqual('zoro.jpg');
+  });
 });
